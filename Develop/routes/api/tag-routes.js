7ec0038f3845f2c52f9f5e5894282b9b9f4e4293
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-03-31 21:34:42 -0700

    updated almost everything and lost track of everything I did (will commit more in the future to prevent this issue)' ' ''
    clear

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,28 +1,90 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  console.log("Read all tags");
+  Tag.findAll({
+    include: [Product],
+  })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(error);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  console.log("Read tag by id");
+  Tag.findOne({
+    where: { id: req.params.id },
+    include: [Product],
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag found with this id." });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(error);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  console.log("Create tag");
   // create a new tag
+  Tag.create(req.body)
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(error);
+      res.status(500).json(err);
+    });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  console.log("Update tag by id");
+  Tag.update(req.body, {
+    where: { id: req.params.id },
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag found with this id" });
+        return;
+      }
+      res.json({ message: "Tag updated successfully." });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  console.log("Delete tag by id");
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tag found with this id." });
+        return;
+      }
+      res.json({ message: "Tag deleted successfully." });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -1,24 +1,24 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+const seedCategories = require("./category-seeds");
+const seedProducts = require("./product-seeds");
+const seedTags = require("./tag-seeds");
+const seedProductTags = require("./product-tag-seeds");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  console.log("\nDatabase Synced\n");
   await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  console.log("\nCatedory Seeded\n");
 
   await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+  console.log("\nProduct Seeded\n");
 
   await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+  console.log("\nTags seeded\n");
 
   await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+  console.log("\nProduct tags seeded\n");
 
   process.exit(0);
 };
