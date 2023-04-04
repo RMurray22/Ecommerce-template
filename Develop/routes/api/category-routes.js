101dad9c34fa3b82362cbebe748bff4dcaba3dc4
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-04-03 18:23:18 -0700

    updated in code

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,15 +4,12 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-  console.log("Read All Categories");
   Category.findAll({
     include: [Product],
   })
     .then((dbCategoryData) => res.json(dbCategoryData))
-    .cath((err) => {
-      console.log(error);
+    .catch((err) => {
+      console.log(err);
       res.status(500).json(err);
     });
 });
